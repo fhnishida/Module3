e19a567e340fa5b04d2518e309be075665773b70
fhnishida <fhnishida@gmail.com>
2020-12-05 11:55:51 -0300

    batch report

--- script1.R
+++ script1.R
@@ -13,3 +13,35 @@ ggplot(sdat,
     ggtitle(paste0("Steak Preparation Preference by Gender for ","Mountain"," Region"))
 
 lm(steak ~ age, data = steak_survey)
+
+
+## RENDER REPORTS
+
+# R code for the render function
+rmarkdown::render("steakParams.Rmd",
+                  params = list(region = "Mountain"))
+
+
+# R code to create the custom function
+render_report <-function(regionvar){
+    template <-"steakArticleParams.Rmd"
+    outfile <-sprintf("steakArticle_%s.html",regionvar)
+    parameters <-list(region = regionvar)
+    rmarkdown::render(template,
+                      output_file=outfile,
+                      params=parameters)
+    invisible(TRUE)
+}
+
+render_report("Pacific")
+
+
+# R code to use the custom function with purrr
+library(purrr)
+params_list <- list(list("East North Central",
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list,render_report)
+
